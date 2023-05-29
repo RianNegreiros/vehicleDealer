ae091ca85e951e3860f68ead8f53e9eba7278433
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-05-29 17:07:31 -0300

    add validation to create vehicle

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -22,6 +22,16 @@ namespace vehicle_retailer.Controllers
     [HttpPost]
     public async Task<IActionResult> CreateVehicle([FromBody] VehicleResource vehicleResource)
     {
+      if (!ModelState.IsValid)
+        return BadRequest(ModelState);
+
+      var model = await _context.Models.FindAsync(vehicleResource.ModelId);
+      if (model == null)
+      {
+        ModelState.AddModelError("ModelId", "Invalid modelId.");
+        return BadRequest(ModelState);
+      }
+
       var vehicle = _mapper.Map<VehicleResource, Vehicle>(vehicleResource);
       vehicle.LastUpdate = DateTime.Now;
       _context.Vehicles.Add(vehicle);
--- Persistence/ApiDbContext.cs
+++ Persistence/ApiDbContext.cs
@@ -7,6 +7,7 @@ namespace vehicle_retailer.Persistence
   {
     public DbSet<Vehicle> Vehicles { get; set; }
     public DbSet<Make> Makes { get; set; }
+    public DbSet<Model> Models { get; set; }
     public DbSet<Feature> Features { get; set; }
 
     public ApiDbContext(DbContextOptions<ApiDbContext> options)
