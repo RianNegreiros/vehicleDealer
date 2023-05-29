bb9fae721e5a1ff00b707e1aa5ea865d79fd9d20
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-05-29 17:19:23 -0300

    add update vehicle endpoint

--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -1,5 +1,6 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using vehicle_retailer.Controllers.Resources;
 using vehicle_retailer.Models;
 using vehicle_retailer.Persistence;
@@ -25,19 +26,35 @@ namespace vehicle_retailer.Controllers
       if (!ModelState.IsValid)
         return BadRequest(ModelState);
 
-      var model = await _context.Models.FindAsync(vehicleResource.ModelId);
-      if (model == null)
-      {
-        ModelState.AddModelError("ModelId", "Invalid modelId.");
-        return BadRequest(ModelState);
-      }
-
       var vehicle = _mapper.Map<VehicleResource, Vehicle>(vehicleResource);
       vehicle.LastUpdate = DateTime.Now;
+
       _context.Vehicles.Add(vehicle);
       await _context.SaveChangesAsync();
 
       var result = _mapper.Map<Vehicle, VehicleResource>(vehicle);
+
+      return Ok(result);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateVehicle(int id, [FromBody] VehicleResource vehicleResource)
+    {
+      if (!ModelState.IsValid)
+        return BadRequest(ModelState);
+
+      var vehicle = await _context.Vehicles.Include(v => v.Features).SingleOrDefaultAsync(v => v.Id == id);
+
+      if (vehicle == null)
+        return NotFound();
+
+      _mapper.Map<VehicleResource, Vehicle>(vehicleResource, vehicle);
+      vehicle.LastUpdate = DateTime.Now;
+
+      await _context.SaveChangesAsync();
+
+      var result = _mapper.Map<Vehicle, VehicleResource>(vehicle);
+
       return Ok(result);
     }
   }
