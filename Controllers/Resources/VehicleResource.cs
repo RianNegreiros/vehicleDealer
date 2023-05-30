0f59babe006b85d7d4d6f1147cdbe40248595711
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-05-30 09:38:12 -0300

    separate resources

--- Controllers/Resources/VehicleResource.cs
+++ Controllers/Resources/VehicleResource.cs
@@ -5,14 +5,16 @@ namespace vehicle_retailer.Controllers.Resources
   public class VehicleResource
   {
     public int Id { get; set; }
-    public int ModelId { get; set; }
+    public ModelResource Model { get; set; }
+    public MakeResource Make { get; set; }
     public bool IsRegistered { get; set; }
     public ContactResource Contact { get; set; }
-    public ICollection<int> Features { get; set; }
+    public DateTime LastUpdate { get; set; }
+    public ICollection<FeatureResource> Features { get; set; }
 
     public VehicleResource()
     {
-      Features = new Collection<int>();
+      Features = new Collection<FeatureResource>();
     }
   }
 }
--- Controllers/VehiclesController.cs
+++ Controllers/VehiclesController.cs
@@ -75,8 +75,12 @@ namespace vehicle_retailer.Controllers
     [HttpGet("{id}")]
     public async Task<IActionResult> GetVehicle(int id)
     {
-      var vehicle = await _context.Vehicles.Include(v => v.Features).SingleOrDefaultAsync(v => v.Id == id);
-
+      var vehicle = await _context.Vehicles
+        .Include(v => v.Features)
+          .ThenInclude(vf => vf.Feature)
+        .Include(v => v.Model)
+          .ThenInclude(m => m.Make)
+        .SingleOrDefaultAsync(v => v.Id == id);
       if (vehicle == null)
         return NotFound();
 
