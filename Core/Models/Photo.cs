6dc9fc97e9b2b70656a80dc6e5b4228754bdf7d0
Rian Negreiros Dos Santos <riannegreiros@gmail.com>
2023-05-30 12:27:40 -0300

    add photo model

---
+++ Core/Models/Photo.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace vehicle_retailer.Core.Models
+{
+  public class Photo
+  {
+    public int Id { get; set; }
+    [Required]
+    [StringLength(255)]
+    public string FileName { get; set; }
+  }
+}
